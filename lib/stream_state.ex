668851eaf8ad82dee0f789eedf2ba7790b1f26ff
Klaus Alfert <klaus.alfert@gmail.com>
2017-09-15 15:59:12 +0200

    Initial commit after mix new

---
+++ lib/stream_state.ex
@@ -0,0 +1,18 @@
+defmodule StreamState do
+  @moduledoc """
+  Documentation for StreamState.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StreamState.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/stream_state_test.exs
@@ -0,0 +1,8 @@
+defmodule StreamStateTest do
+  use ExUnit.Case
+  doctest StreamState
+
+  test "greets the world" do
+    assert StreamState.hello() == :world
+  end
+end
