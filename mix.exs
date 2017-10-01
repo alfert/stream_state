e227a23ce71828c75c05b137d5ff025626895ee9
Klaus Alfert <klaus.alfert@gmail.com>
2017-10-01 19:53:53 +0200

    Include the test/support directory for test source code

--- mix.exs
+++ mix.exs
@@ -6,11 +6,17 @@ defmodule StreamState.Mixfile do
       app: :stream_state,
       version: "0.1.0",
       elixir: "~> 1.5",
+      elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
       test_coverage: [tool: Coverex.Task, console_log: true],
       deps: deps()
     ]
   end
+
+  # Specifies which paths to compile per environment
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
 
   # Run "mix help compile.app" to learn about applications.
   def application do
