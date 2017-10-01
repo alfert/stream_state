7c1e35282bd5a705e447cd3b6c846ab18090178a
Klaus Alfert <klaus.alfert@gmail.com>
2017-10-01 13:22:22 +0200

    Update to StreamData 0.3

--- test/stream_state_test.exs
+++ test/stream_state_test.exs
@@ -2,7 +2,16 @@ defmodule StreamStateTest do
   use ExUnit.Case
   doctest StreamState
 
+  use ExUnitProperties
+
   test "greets the world" do
     assert StreamState.hello() == :world
   end
+
+  property "reversing a list doesn't change its length" do
+    check all list <- list_of(integer()) do
+      assert length(list) == length(:lists.reverse(list))
+    end
+  end
+
 end
