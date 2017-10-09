d196f92d5ab1c629a4edb246460e5b4beeec69e2
Klaus Alfert <klaus.alfert@gmail.com>
2017-10-09 23:09:27 +0200

    Define a macro to generate a symbolic call

--- lib/stream_state.ex
+++ lib/stream_state.ex
@@ -3,16 +3,27 @@ defmodule StreamState do
   Documentation for StreamState.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> StreamState.hello
-      :world
-
-  """
-  def hello do
-    :world
+  defmacro __using__(_) do
+    quote do
+      import StreamState
+    end
   end
+
+  defmacro call(fun_call = {{:., _, _mod_list}, _, _args} ) do
+    call = Macro.decompose_call(fun_call)
+    mfa = Macro.escape {module(call), function(call), args(call)}
+    quote do {:call, unquote(mfa)} end
+  end
+
+  def module({_fun, _args}), do: Kernel
+  def module({{:__aliases__, [alias: false], mods}, _, _}), do: Module.concat(mods)
+  def module({{:__aliases__, [alias: mods], _aliased_mod}, _, _}), do: Module.concat(mods)
+  def module({{:__aliases__, _, mods}, _, _}), do: Module.concat(mods)
+
+  def function({fun, _args}), do: fun
+  def function({{:__aliases__, _, _}, fun, _args}), do: fun
+
+  def args({_fun, args}), do: args
+  def args({{:__aliases__, _, _}, _fun, args}), do: args
+
 end
