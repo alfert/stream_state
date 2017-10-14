fb6ce52ddee1a54e2c037c51a8f776c60557837f
Klaus Alfert <klaus.alfert@gmail.com>
2017-10-14 16:38:30 +0200

    Study the `bind` function

--- test/stream_state_test.exs
+++ test/stream_state_test.exs
@@ -5,7 +5,6 @@ defmodule StreamStateTest do
   use StreamState
 
   test "call macro with a proper call" do
-
     mfa = call StreamState.run_commands([1, 2, 3])
     assert mfa == {:call, {StreamState, :run_commands, [[1, 2, 3]]}}
   end
@@ -14,6 +13,19 @@ defmodule StreamStateTest do
     check all list <- list_of(integer()) do
       assert length(list) == length(:lists.reverse(list))
     end
+  end
+
+  test "use bind to produce pairs of lists and a member" do
+    pairs = StreamData.integer()
+    |> StreamData.list_of()
+    |> StreamData.nonempty()
+    |> StreamData.bind(fn list ->
+      list
+      |> StreamData.member_of()
+      |> StreamData.bind(fn elem -> StreamData.constant({list, elem}) end)
+    end)
+    |> Enum.take(10)
+    assert pairs |> Enum.all?(fn {l, m} -> Enum.member?(l, m) end)
   end
 
 end
